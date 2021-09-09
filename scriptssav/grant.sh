9a6f3a870bf3318e40187376a004ce5fe3934849
prathjan <prathjan@cisco.com>
2021-09-09 15:02:22 -0700

    deleted

--- scriptssav/grant.sh
+++
@@ -1,9 +0,0 @@
-dbhost=$2
-vmip=$1
-db=$3
-user=$4
-pass=$5
-mysql -uroot -proot <<MYSQL_SCRIPT
-GRANT ALL PRIVILEGES ON ${db}.* TO '${user}'@'${vmip}' IDENTIFIED BY '${pass}';
-FLUSH PRIVILEGES;
-MYSQL_SCRIPT
--- scriptssav/startup.sh
+++
@@ -1,4 +0,0 @@
-export CATALINA_HOME=/usr/local/apache/apache-tomcat-7
-export CATALINA_BASE=/usr/local/apache/svcname
-/usr/local/apache/apache-tomcat-7/bin/startup.sh
-
