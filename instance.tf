aed04754242fc3de3539449bff477cacd31293b4
prathjan <prathjan@cisco.com>
2021-09-28 19:19:14 -0700

    use var.org

--- instance.tf
+++ instance.tf
@@ -13,7 +13,7 @@ data "terraform_remote_state" "global" {
 data "terraform_remote_state" "dbvm" {
   backend = "remote"
   config = {
-    organization = "Lab14"
+    organization = var.org
     workspaces = {
       name = var.dbvmwsname
     }
